a2da6afca3cfa9c246165f44a13f70dd684f3826
Nazarii <nazik.klas.opaopaopapa@gmail.com>
2025-10-02 00:32:55 +0300

    enabled headless mode

--- src/crawler/services/crawlerBrowserManager.service.ts
+++ src/crawler/services/crawlerBrowserManager.service.ts
@@ -13,7 +13,7 @@ export class CrawlerBrowserManagerService {
 
   async createBrowserInstance() {
     return await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: [
         '--disable-background-timer-throttling',
         '--disable-backgrounding-occluded-windows',
