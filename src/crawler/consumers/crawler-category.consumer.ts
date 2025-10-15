f2e674ea22be32705023dce1290adf8fda42b7cc
Nazarii <nazik.klas.opaopaopapa@gmail.com>
2025-10-15 14:13:07 +0300

    reduced concurency

--- src/crawler/consumers/crawler-category.consumer.ts
+++ src/crawler/consumers/crawler-category.consumer.ts
@@ -15,7 +15,7 @@ import { CrawlerRequest } from '../entities/crawl-request.entity';
 @Processor(CRAWLER_QUEUES.CATEGORY_LINKS, {
   lockDuration: 2 * 60 * 60 * 1000,
   maxStalledCount: 2,
-  concurrency: 5,
+  concurrency: 2,
 })
 export class CrawlerCategoryConsumer extends WorkerHost {
   constructor(
